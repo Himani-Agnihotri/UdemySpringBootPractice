52874886bb6219c1737700c0e39b2294217df55f
himani-agnihotri <himani.agnihotri@newgensoft.com>
2025-07-12 13:32:59 +0530

    added changes

---
+++ src/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Main extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello from Servlet</h1>");
+    }
+}
